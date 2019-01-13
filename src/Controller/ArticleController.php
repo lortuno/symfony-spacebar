38923d4866345c03f86fb8c7168b49d306d1dfbc
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-01-13 22:28:50 +0100

    Adding articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+	/**
+	 * @Route("/")
+	 */
 	public function homepage()
 	{
 		return new Response('Epa!');
+	}
+
+	/**
+	 * @Route("/news/{$articleName")
+	 */
+	public function show($articleName)
+	{
+		return new Response('This is gonna be my article named: ' . $articleName);
 	}
 }
