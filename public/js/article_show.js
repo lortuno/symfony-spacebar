d7600af5533c875915a97512e81f357776121274
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-01-18 10:15:53 +0100

    #master 11.- Modifica js y añade autosave.

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -1,12 +1,12 @@
-jQuery(document.ready(function ($) {
-	$('.e-like-article').on('click', function (e) {
-		e.preventDefault();
+jQuery(document).ready(
+	function ($) {
+		$('.e-like-article').on('click', function (e) {
+			e.preventDefault();
 
-		var $link = $(e.currentTarget),
-			$articleCounter = '.rw-like-article-count';
+			var $link = $(e.currentTarget),
+				$articleCounter = '.rw-like-article-count';
 
-		$link.toggleClass('fa-heart-o').toggleClass('fa-heart');
-		$($articleCounter).html('test');
+			$link.toggleClass('fa-heart-o').toggleClass('fa-heart');
+			$($articleCounter).html('test');
+		});
 	});
-})
-);
