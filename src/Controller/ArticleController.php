3b867e14606834a3d32c50b107b2776d8927d1c0
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-02-12 10:26:49 +0100

    #master phase II Quito etiquetas PRE code.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -30,7 +30,7 @@ class ArticleController extends AbstractController
             'It will be awesome when this has css',
         ];
 
-        $articleContent = <<<EOF
+        $articleContent = "
             Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
             lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
             labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
@@ -44,8 +44,7 @@ class ArticleController extends AbstractController
             mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
             strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
             cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
-            fugiat.
-EOF;
+            fugiat.";
 
         $articleContent = $markdown->transform($articleContent);
 
